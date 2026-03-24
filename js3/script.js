507f73ccfa0922b598b5db4615a17f1b86508b78
Arindam Pandey <arindampandey63@gmail.com>
2026-03-24 13:19:01 +0530

    Add files via upload

---
+++ js3/script.js
@@ -0,0 +1,25 @@
+// Conditionals in JS
+console.log("Hello I am conditional tutorial")
+
+let age = 3;
+grace = 2;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+
+// console.log(age ** grace) // (** is exponential operatot which is used to get power of a number )
+
+if ((age + grace) > 18) {
+    console.log("You can drive");
+}
+
+else {
+    console.log("You can not drive")
+}
+
+// === is used to check equal value and type
+if("three"==="three"){
+    console.log("True")
+}
