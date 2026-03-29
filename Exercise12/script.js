1e1b8a4c67283d4cfa35d2cce1f0941a35c73ed5
Arindam Pandey <arindampandey63@gmail.com>
2026-03-29 22:18:57 +0530

    Update script.js

--- Exercise12/script.js
+++ Exercise12/script.js
@@ -5,15 +5,18 @@
 
 // let color = `rgb(${r}, ${g}, ${b})`;
 
+function getrandomcolor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
+}
 let boxes = document.getElementsByClassName("box");
 console.log(boxes);
 
 let i;
 for (i = 0; i < 5; i++) {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    let color = `rgb(${r}, ${g}, ${b})`;
-
-    boxes[i].style.backgroundColor = color;
+    boxes[i].style.backgroundColor = getrandomcolor();
+    boxes[i].style.color = getrandomcolor();
 }
