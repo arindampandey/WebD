4e54ba37f517b6d1b26cf52224470df1fee2de30
Arindam Pandey <arindampandey63@gmail.com>
2026-04-12 11:55:42 +0530

    Add files via upload

---
+++ BE5/main.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require("fs")
+const blogs = require('./routes/blogs')
+
+// app.use(express.static("public"))
+
+app.use('/blogs',blogs)
+
+//Middleware 1
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.myrequest = "Hello Sir"
+
+    fs.appendFileSync("new.txt", `${Date.now()} is a ${req.method}\n`)
+    console.log(`${Date.now()} is a ${req.method}`)
+    // res.send("Middleware 1")
+    next() // if next is not used then no response , server will stuck .
+})
+
+//Middleware 2
+app.use((req, res, next) => {
+    req.myrequest = "Byee Sir"
+
+    console.log('m2')
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('Hello about!'+ req.myrequest)
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Hello contact!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
