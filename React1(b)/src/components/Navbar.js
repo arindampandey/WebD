b716d4c82b0495683b75cc1322d37818decc05c8
Arindam Pandey <arindampandey63@gmail.com>
2026-04-25 13:51:13 +0530

    Add files via upload

---
+++ React1(b)/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <div className="logo">{props.logoText}</div>
+      <ul>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Navbar
