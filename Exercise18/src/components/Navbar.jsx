02285c61d15f5cb7fc1225816869845a59aa0735
Arindam Pandey <arindampandey63@gmail.com>
2026-04-27 10:41:02 +0530

    Add files via upload

---
+++ Exercise18/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
