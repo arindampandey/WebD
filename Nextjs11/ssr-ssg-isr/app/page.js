fa3feef91c5fd1ac4ed4672af1c0da96ebfd707e
Arindam Pandey <arindampandey63@gmail.com>
2026-05-23 12:26:15 +0530

    Add files via upload

---
+++ Nextjs11/ssr-ssg-isr/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default async function Page() {
+  const data = await fetch('https://api.vercel.app/blog',{cache : 'no-store '})
+  const posts = await data.json()
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
+}
